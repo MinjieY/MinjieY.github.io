fd16eedb89a8295781a1e53185f64c35387ea236
MinjieY <yingminjie@tom.com>
2024-10-15 10:59:27 +0800

    Site updated: 2024-10-15 10:59:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/4960c6d9.html","post/deab04e1.html","post/1e432c02.html","post/0.html","post/7c8327f0.html","post/c3f5278c.html"];function toRandomPost(){
+var posts=["post/4960c6d9.html","post/deab04e1.html","post/1e432c02.html","post/0.html","post/7c8327f0.html","post/c3f5278c.html","post/15f4d9b2.html","post/15f4d9b2.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
